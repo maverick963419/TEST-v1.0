4b40cf11adc62e525463c47f048fa562d9aeaa29
maverick963419 <45982601+maverick963419@users.noreply.github.com>
2020-06-06 08:10:48 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,5 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lancenas/lienol-openwrt-package.git' feeds.conf.default
